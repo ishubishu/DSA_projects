d1022f5eca1817f973a9438e145052607d864ef8
khuss2432 <60210511+khuss2432@users.noreply.github.com>
2022-05-03 21:05:12 +0530

    Create sol4.cpp

---
+++ Assignment 2/sol4.cpp
@@ -0,0 +1,13 @@
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int size;
+    cin>>size;
+    int *arr= new int[size];
+    for (int i=0;i<=size;i++){
+        cin>>arr[i];
+    }  
+    for(int i=(size-1);i>=0;i--){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
