adc378471e42ec23b103ecf5981bab0d0ac5faa3
khuss2432 <60210511+khuss2432@users.noreply.github.com>
2022-05-03 13:25:09 +0530

    Create sol3.cpp

---
+++ Assignment1/sol3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    long l;
+    char c;
+    float f;
+    double d;
+    cin>>n>>l>>c>>f>>d;
+    cout<<n<<'\n'<<l<<'\n'<<c<<'\n';
+    cout << std::fixed << std::setprecision(3) << f<<'\n';
+    cout << std::fixed << std::setprecision(9) <<d << '\n';
+    return 0;
+}
