2e775ac71e73f345f896d2276c1da52e6a2b3748
khuss2432 <60210511+khuss2432@users.noreply.github.com>
2022-05-03 21:58:47 +0530

    Update sol6.cpp

--- Assignment 2/sol6.cpp
+++ Assignment 2/sol6.cpp
@@ -1,1 +1,23 @@
 
+vector<int>getTwoLargestNumbers(vector<int>&A){
+    int max1=INT_MIN, max2=INT_MIN;
+    int count=0,count2=0;
+    //write your code to find max1 and max2
+    for(int i=0;i<A.size();i++){
+        if(max1<A[i]){
+            max2=max1;
+            max1=A[i];
+        }else{
+            if(max1==A[i])
+                max2=max1;
+        else if(max1>A[i]&&max2<A[i]){
+            max2=A[i];
+        }
+        
+    }   
+        
+    }
+    
+    
+    return {max1,max2};
+}
