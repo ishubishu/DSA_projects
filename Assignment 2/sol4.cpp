1d2e69b3ece8334871c06e6de7d473a1d8f3763d
khuss2432 <60210511+khuss2432@users.noreply.github.com>
2022-05-05 00:46:47 +0530

    Update sol4.cpp

--- Assignment 2/sol4.cpp
+++ Assignment 2/sol4.cpp
@@ -1,12 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    
+    vector<int> arr;
     int size;
+    int a;
     cin>>size;
-    int *arr= new int[size];
-    for (int i=0;i<=size;i++){
-        cin>>arr[i];
+    for (int i=0;i<size;i++){
+        cin>>a;
+        arr.push_back(a);
     }  
-    for(int i=(size-1);i>=0;i--){
+    for(int i=size-1;i>=0;i--){
         cout<<arr[i]<<" ";
     }
     return 0;
