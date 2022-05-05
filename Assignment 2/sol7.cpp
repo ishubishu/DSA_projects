07d978923dd49a288a99869c78350bb93fcc380f
khuss2432 <60210511+khuss2432@users.noreply.github.com>
2022-05-05 23:45:28 +0530

    Update sol7.cpp

--- Assignment 2/sol7.cpp
+++ Assignment 2/sol7.cpp
@@ -5,7 +5,7 @@ bool isPrime(int a){
     if(a<=1){
         return false;
     }
-    for (int i = 2; i < sqrt(a); i++)
+    for (int i = 2; i <=sqrt(a); i++)
         if (a % i == 0)
             return false;
  
@@ -14,7 +14,7 @@ bool isPrime(int a){
 int main() {
     int a;
     cin>>a;
-    for(int i=1;i<sqrt(a);i++){
+    for(int i=1;i<a;i++){
          if (isPrime(i)){
             cout<<"the number "<<i<<" is prime\n";
          }
