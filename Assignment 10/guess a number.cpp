6b2d94c068ba782abc8a50361cf5d68ffccc03c2
khuss2432 <60210511+khuss2432@users.noreply.github.com>
2022-05-16 19:53:54 +0530

    Create guess a number.cpp

---
+++ Assignment 10/guess a number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    typedef unsigned long long ULL;
+    int guessNumber(ULL n) {
+        ULL g;
+        ULL beg=1,end=n;
+        while(beg<=end){
+            ULL mid=(beg+end)/2;
+            g=guess(mid);
+            if(g==0){
+                return mid;
+            }
+            else if(g==-1){
+                end=mid-1;
+            }
+            else{
+                beg=mid+1;
+            }
+        }
+        return 0;
+    
+            
+        
+        
+    }
+};
